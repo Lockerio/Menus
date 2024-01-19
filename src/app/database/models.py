978a7117d6d9bfb8f56f35bdffbd87e867d88655
Arkadiy <goryunov.arkadiy@gmail.com>
2024-01-19 22:54:42 +0700

    Fix ids, create tables

--- src/app/database/models.py
+++ src/app/database/models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+import uuid
+
+from sqlalchemy import Column, Integer, String, ForeignKey, UUID
 from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
@@ -7,7 +9,7 @@ Base = declarative_base()
 class Menu(Base):
     __tablename__ = "menus"
 
-    id = Column(Integer(), primary_key=True, autoincrement=True, nullable=False)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, nullable=False)
     title = Column(String(), nullable=False)
     description = Column(String(), nullable=False)
 
@@ -17,10 +19,10 @@ class Menu(Base):
 class Submenu(Base):
     __tablename__ = "submenus"
 
-    id = Column(Integer(), primary_key=True, autoincrement=True, nullable=False)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, nullable=False)
     title = Column(String(), nullable=False)
     description = Column(String(), nullable=False)
-    menu_id = Column(Integer(), ForeignKey("menus.id"))
+    menu_id = Column(UUID(), ForeignKey("menus.id"))
 
     menu = relationship("Menu", back_populates="submenus")
     dishes = relationship("Dish", back_populates="submenu")
@@ -29,10 +31,10 @@ class Submenu(Base):
 class Dish(Base):
     __tablename__ = "dishes"
 
-    id = Column(Integer(), primary_key=True, autoincrement=True, nullable=False)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, nullable=False)
     title = Column(String(), nullable=False)
     description = Column(String(), nullable=False)
     price = Column(String(), nullable=False)
-    submenu_id = Column(Integer(), ForeignKey("submenus.id"))
+    submenu_id = Column(UUID(), ForeignKey("submenus.id"))
 
     submenu = relationship("Submenu", back_populates="dishes")
